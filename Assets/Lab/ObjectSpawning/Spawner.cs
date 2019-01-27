97bd26a56f4cefd60d44c907ab3620f211394ef1
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-01-27 17:06:53 +0000

    Add experiment to spawn resources

---
+++ Assets/Lab/ObjectSpawning/Spawner.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+
+namespace Lab.ObjectSpawning
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject spawnPoint;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            for (int i = 0; i < 30; i++)
+            {
+                SpawnTree();
+            }
+        }
+
+        private void SpawnTree()
+        {
+            var pos = Random.insideUnitSphere * 50;
+
+            pos.y = 0;
+
+            var tree = Instantiate(
+                Resources.Load("BroadLeafTree") as GameObject,
+                pos + spawnPoint.transform.position,
+                Quaternion.identity);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
