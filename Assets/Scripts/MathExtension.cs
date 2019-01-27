8b14a1b8647ae36715b9c2503a16150f8494117e
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-01-27 20:33:35 +0000

    Add Gaussian distribution functions

--- Assets/Scripts/MathExtension.cs
+++ Assets/Scripts/MathExtension.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public static class MathExtension
 {
     public static float Remap(this float from, float fromMin, float fromMax, float toMin, float toMax)
@@ -18,5 +20,19 @@ public static class MathExtension
     public static float Remap(this int from, float fromMin, float fromMax, float toMin, float toMax)
     {
         return ((float) from).Remap(fromMin, fromMax, toMin, toMax);
+    }
+
+    public static float GaussianDistribution(float x, float mean, float sigma)
+    {
+        var a = Mathf.Sqrt(2f * Mathf.PI * sigma * sigma);
+        return 1f / a * GaussianProbability(x, mean, sigma);
+    }
+
+    public static float GaussianProbability(float x, float mean, float sigma)
+    {
+        var b = -Mathf.Pow(x - mean, 2);
+        var c = 2f * sigma * sigma;
+
+        return Mathf.Exp(b / c);
     }
 }
