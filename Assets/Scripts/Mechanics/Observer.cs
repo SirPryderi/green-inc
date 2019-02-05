79e00fdd9bfd92ff0f43b4213e1d15f8b585eb2c
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-05 18:49:01 +0000

    Add delta time to observer

--- Assets/Scripts/Mechanics/Observer.cs
+++ Assets/Scripts/Mechanics/Observer.cs
@@ -6,37 +6,41 @@ namespace Mechanics
 {
     public class Observer
     {
-        private readonly List<IObservable> observables;
+        private readonly List<IObservable> _observables;
         public int Time { get; private set; }
+        public int DeltaTime { get; private set; }
 
         public Observer()
         {
-            observables = new List<IObservable>();
+            _observables = new List<IObservable>();
         }
 
         public void Register(IObservable observable)
         {
-            observables.Add(observable);
+            _observables.Add(observable);
         }
 
         public void UnRegister(IObservable observable)
         {
-            observables.Remove(observable);
+            _observables.Remove(observable);
         }
 
         public void AdvanceTime(int hours)
         {
             Time += hours;
-            
-            foreach (var observable in observables)
+            DeltaTime = hours;
+
+            foreach (var observable in _observables)
             {
                 observable.StartFrame();
             }
 
-            foreach (var observable in observables)
+            foreach (var observable in _observables)
             {
                 observable.EndFrame();
             }
+            
+            Object.FindObjectOfType<HexGrid>().Refresh();
         }
     }
 }
