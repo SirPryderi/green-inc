cd0aeaaaf3aaf38cb8e38fbad7aab46aab274470
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-06 01:28:04 +0000

    Add more companies

--- Assets/Scripts/MapManager.cs
+++ Assets/Scripts/MapManager.cs
@@ -20,8 +20,9 @@ public class MapManager
         RandomGenerator = new RandomGenerator(0);
 
         Observer = new Observer();
-        Orgs.PlayerCompany = new Company("Green Inc.", CompanyType.GENERIC, 5000);
-        Orgs.Companies.Add(new Company("Black Inc.", CompanyType.ELECTRICAL));
+        Orgs.PlayerCompany = new Company("Green Inc.", CompanyType.GENERIC, 5_000);
+        Orgs.Companies.Add(new Company("Energy Inc.", CompanyType.ELECTRICAL, 10_000));
+        Orgs.Companies.Add(new Company("Food Inc.", CompanyType.FOOD, 10_000));
     }
 
     public void Randomise()
