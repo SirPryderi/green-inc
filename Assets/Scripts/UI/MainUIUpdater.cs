1af07d9dd4312f2aab359324b662f0044419cff7
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-07 20:04:43 +0000

    Update MainUI to show additional statistics

--- Assets/Scripts/UI/MainUIUpdater.cs
+++ Assets/Scripts/UI/MainUIUpdater.cs
@@ -1,5 +1,6 @@
 using System;
 using Mechanics;
+using Stat;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,6 +20,9 @@ namespace UI
 
         public Text CO2Budget;
         public Text CO2Income;
+
+        public Text Population;
+        public Text AverageTemperature;
 
         public MainUIController MainUIController;
 
@@ -42,19 +46,66 @@ namespace UI
             UpdateClock();
             UpdateCO2();
             UpdateBalance();
+            UpdatePopulation();
+        }
+
+        private void UpdatePopulation()
+        {
+            Population.text = $"{Statistics.Population:##,###0}";
+            AverageTemperature.text = $"{Statistics.AverageTemperature:##,###0.00 °C}";
         }
 
         private void UpdateBalance()
         {
+            // Amount
             var budget = G.PC.Money;
             MoneyBudget.text = $"$ {budget:##,###0}";
+
+            // Variation
+            var current = G.MP.Statistics.Data.Last;
+
+            if (current?.Previous == null)
+            {
+                MoneyIncome.text = "-/DAY";
+
+                return;
+            }
+
+            var previous = current.Previous;
+
+            var deltaTime = current.Value.time - previous.Value.time;
+            var deltaMoney = current.Value.money - previous.Value.money;
+
+            var increase = deltaMoney / deltaTime * 24;
+
+            MoneyIncome.text = $"{increase:+##,###0}/DAY";
         }
 
         private void UpdateCO2()
         {
+            // Amount
             var valueF = G.CM.Atmosphere.GetGasPercentage("Carbon Dioxide") * 1_000_000f;
             var value = Convert.ToInt32(valueF);
             CO2Budget.text = $"CO₂ {value:##,###} <size=10>ppm</size>";
+
+            // Variation
+            var current = G.MP.Statistics.Data.Last;
+
+            if (current?.Previous == null)
+            {
+                CO2Income.text = "-/DAY";
+
+                return;
+            }
+
+            var previous = current.Previous;
+
+            var deltaTime = current.Value.time - previous.Value.time;
+            var deltaMoney = current.Value.carbonDioxideConcentration - previous.Value.carbonDioxideConcentration;
+
+            var increase = deltaMoney / deltaTime * 24 * 365 * 1_000_000;
+
+            CO2Income.text = $"{increase:+##,###0.00}/YEAR";
         }
 
         public void UpdateClock()
