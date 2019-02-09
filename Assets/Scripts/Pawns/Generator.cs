3bbc05176b5729de28f5f57cff11a6db94f0c2ff
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-09 19:23:55 +0000

    Add generator class

---
+++ Assets/Scripts/Pawns/Generator.cs
@@ -0,0 +1,64 @@
+using System;
+using Items;
+using Mechanics;
+using Organisations;
+using UnityEditor;
+
+namespace Pawns
+{
+    public class Generator : Pawn, IObservable
+    {
+        public Item item;
+
+        public uint itemsPerHour;
+        public float pricePerItem;
+        public float co2PerItem;
+
+        public uint ProvidedInPastFrame;
+
+        private uint provided;
+
+        public void StartFrame()
+        {
+        }
+
+        public void EndFrame()
+        {
+            ProvidedInPastFrame = provided;
+            provided = 0;
+        }
+
+        public uint ProduceItemsFor(uint amount, Organisation org)
+        {
+            provided += amount;
+
+            var providedLimit = Convert.ToUInt32(itemsPerHour * G.DeltaTime);
+
+            if (provided > providedLimit)
+            {
+                amount = amount - (provided - providedLimit);
+                provided = providedLimit;
+            }
+
+            if (amount == 0)
+            {
+                return 0;
+            }
+
+            var total = Convert.ToInt32(amount * pricePerItem);
+            org.TransferMoney(owner, total);
+
+            G.CM.Atmosphere.ReleaseGas("Carbon Dioxide", amount * co2PerItem);
+
+            return amount;
+        }
+
+#if UNITY_EDITOR
+        void OnDrawGizmos()
+        {
+            if (G.DeltaTime != 0)
+                Handles.Label(transform.position, $"{ProvidedInPastFrame / 1000 / G.DeltaTime} kW");
+        }
+#endif
+    }
+}
