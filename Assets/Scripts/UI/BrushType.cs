62bbb1eb265131ab7fa971ba30d55cb69a8b6427
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-10 16:21:57 +0000

    Add place pawn mechanics

---
+++ Assets/Scripts/UI/BrushType.cs
@@ -0,0 +1,11 @@
+namespace UI
+{
+    public enum BrushType
+    {
+        NONE,
+        BULLDOZER,
+        WIND_TURBINE,
+        COAL_PLANT,
+        TREE,
+    }
+}
--- Assets/Scripts/UI/MainUIController.cs
+++ Assets/Scripts/UI/MainUIController.cs
@@ -1,10 +1,14 @@
+using System.Collections;
 using UnityEngine;
+using UnityEngine.EventSystems;
 
 namespace UI
 {
     public class MainUIController : MonoBehaviour
     {
         public bool isPaused = false;
+
+        private BrushType _brush = BrushType.NONE;
 
         public void TogglePause()
         {
@@ -16,6 +20,79 @@ namespace UI
         public void Advance()
         {
             GameManager.Instance.MapManager.Observer.AdvanceTime(12);
+        }
+
+        public void SetBrush(BrushType brush)
+        {
+            _brush = brush;
+        }
+
+        public void SetBrush(int brush)
+        {
+            SetBrush((BrushType) brush);
+        }
+
+        private void Update()
+        {
+            if (_brush == BrushType.NONE)
+                return;
+
+            if (!EventSystem.current.IsPointerOverGameObject() && Input.GetMouseButtonDown(0))
+            {
+                StartCoroutine(nameof(HandleInput));
+            }
+        }
+
+        private IEnumerator HandleInput()
+        {
+            var inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (!Physics.Raycast(inputRay, out var hit)) yield break;
+
+            var cell = G.MP.Grid.GetCell(hit.point);
+
+            if (_brush == BrushType.BULLDOZER || cell.isClear())
+            {
+                switch (_brush)
+                {
+                    case BrushType.BULLDOZER:
+                        BulldozeCell(cell);
+                        break;
+                    case BrushType.WIND_TURBINE:
+                        BuyPawn(cell, "WindTurbine");
+                        break;
+                    case BrushType.COAL_PLANT:
+                        BuyPawn(cell, "ElectricalPlant");
+                        break;
+                    case BrushType.TREE:
+                        BuyPawn(cell, "PineTree");
+                        break;
+                    case BrushType.NONE:
+                    default:
+                        break;
+                }
+
+                if (!Input.GetKey(KeyCode.LeftShift))
+                {
+                    _brush = BrushType.NONE;
+                }
+            }
+
+            yield return null;
+        }
+
+        private void BuyPawn(HexCell cell, string pawn)
+        {
+            cell.Spawn(pawn, G.PC);
+        }
+
+        private void BulldozeCell(HexCell cell)
+        {
+            if(cell.isClear()) return;
+            
+            // Makes sure you can only bulldoze nobody's or your pawns
+            if (cell.Pawn.owner == null || cell.Pawn.owner == G.PC)
+                cell.Clear();
         }
     }
 }
