f155f97ae992e130b88f50b5ffd293e2beabe3e7
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-14 18:48:28 +0000

    Add utility code to calculate item satisfaction

--- Assets/Scripts/Logistics/Requester.cs
+++ Assets/Scripts/Logistics/Requester.cs
@@ -64,5 +64,39 @@ namespace Logistics
 
             return filter;
         }
+
+        public static IEnumerable<Requester> FindRequesters(Item item)
+        {
+            var result = FindObjectsOfType<Requester>();
+            var filter = new List<Requester>(result.Length);
+
+            filter.AddRange(result.Where(requester => requester.item == item));
+            filter.TrimExcess();
+
+            return filter;
+        }
+
+        public static float ItemSatisfaction(Item item, out float requested, out float satisfied)
+        {
+            var requesters = FindRequesters(item);
+
+            requested = 0f;
+            satisfied = 0f;
+
+            foreach (var requester in requesters)
+            {
+                requested += requester.requested;
+                satisfied += requester.satisfied;
+            }
+
+            var percent = satisfied / requested;
+
+            return Mathf.Clamp01(percent);
+        }
+
+        public static float ItemSatisfaction(Item item)
+        {
+            return ItemSatisfaction(item, out _, out _);
+        }
     }
 }
