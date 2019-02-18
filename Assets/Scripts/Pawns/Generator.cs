a4313cc9a11a75137dfcb174bc87e66a5fbda60a
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-18 20:17:32 +0000

    Change Generator tick updates

--- Assets/Scripts/Pawns/Generator.cs
+++ Assets/Scripts/Pawns/Generator.cs
@@ -14,21 +14,24 @@ namespace Pawns
         public float pricePerItem;
         public float co2PerItem;
 
-        [HideInInspector] public ulong ProvidedInPastFrame;
-
         public float ProductivityPercentage => ProducedInPastHour / itemsPerHour;
-        public float ProducedInPastHour => (float) ProvidedInPastFrame / G.DeltaTime;
+        public float ProducedInPastHour => (float) _provided / G.DeltaTime;
 
         private ulong _provided;
+        private int _lastTime;
 
-        public override void EndFrame()
+        private void CheckNewTick()
         {
-            ProvidedInPastFrame = _provided;
+            if (_lastTime == G.O.Time) return;
+            
+            _lastTime = G.O.Time;
             _provided = 0;
         }
 
         public ulong ProduceItemsFor(ulong amount, Organisation org)
         {
+            CheckNewTick();
+
             _provided += amount;
 
             var providedLimit = Convert.ToUInt64(itemsPerHour * G.DeltaTime);
@@ -56,7 +59,7 @@ namespace Pawns
         void OnDrawGizmos()
         {
             if (G.DeltaTime != 0)
-                Handles.Label(transform.position, $"{ProvidedInPastFrame / 1000ul / (ulong) G.DeltaTime} kW");
+                Handles.Label(transform.position, $"{_provided / 1000ul / (ulong) G.DeltaTime} kW");
         }
 #endif
     }
