f278e5c22ca29100664753f9d64d9714ad870c8b
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-02-24 15:00:02 +0000

    Add resources panel

---
+++ Assets/Scripts/UI/ResourcesWindow/ResourceEntry.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UI.ResourcesWindow
+{
+    public class ResourceEntry : MonoBehaviour
+    {
+        public Image icon;
+        public Text name;
+        public Text requested;
+        public Text produced;
+        public Text capacity;
+    }
+}
---
+++ Assets/Scripts/UI/ResourcesWindow/ResourcesList.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using Items;
+using Logistics;
+using Mechanics;
+using UnityEngine;
+
+namespace UI.ResourcesWindow
+{
+    public class ResourcesList : MonoBehaviour, IObservable
+    {
+        public ResourceEntry rowPrefab;
+        public List<Item> items;
+
+        public Color good = Color.green;
+        public Color bad = Color.red;
+
+        private void Awake()
+        {
+            G.O.Register(this);
+            Clear();
+            Populate();
+        }
+
+        private void OnDestroy()
+        {
+            G.O.UnRegister(this);
+        }
+
+        private void Clear()
+        {
+            foreach (var component in GetComponentsInChildren<ResourceEntry>())
+            {
+                Destroy(component.gameObject);
+            }
+        }
+
+        private void Populate()
+        {
+            foreach (var item in items)
+            {
+                LogisticNetwork.ItemSatisfaction(item, out float requested, out float satisfied);
+                var capacity = LogisticNetwork.ItemCapacity(item).Maximum;
+
+                rowPrefab.icon.sprite = item.sprite;
+                rowPrefab.icon.SetNativeSize();
+                rowPrefab.name.text = item.name;
+
+                requested /= G.DeltaTime;
+                satisfied /= G.DeltaTime;
+
+                rowPrefab.produced.color = satisfied < requested ? bad : good;
+
+                rowPrefab.requested.text = requested.ToString("N");
+                rowPrefab.produced.text = satisfied.ToString("N");
+                rowPrefab.capacity.text = capacity.ToString("N");
+
+                Instantiate(rowPrefab, transform);
+            }
+        }
+
+        public void StartFrame()
+        {
+        }
+
+        public void EndFrame()
+        {
+            Clear();
+            Populate();
+        }
+    }
+}
