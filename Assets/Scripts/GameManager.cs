c1e265608b0b39ac0239cb78dcb94a410a09645e
Vittorio Iocolano <pryderi.mail@gmail.com>
2019-03-26 12:11:09 +0000

    Add hide UI feature

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UI;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -5,6 +7,7 @@ using UnityEngine.SceneManagement;
 public class GameManager : Singleton<GameManager>
 {
     public MapManager MapManager { get; private set; }
+    private bool _uiVisible = true;
 
     protected override void OnAwake()
     {
@@ -34,6 +37,42 @@ public class GameManager : Singleton<GameManager>
                 ToMainMenu();
             }
         }
+
+        if (Input.GetKeyDown("h"))
+        {
+            ToggleUI();
+        }
+    }
+
+    private void ToggleUI()
+    {
+        var sceneName = SceneManager.GetActiveScene().name;
+
+        if (sceneName != "Game" && sceneName != "Scene")
+        {
+            return;
+        }
+
+        _uiVisible = !_uiVisible;
+
+        var objects = FindGameObjectsInLayer(LayerMask.NameToLayer("UI"));
+
+        foreach (var obj in objects)
+        {
+            obj.transform.localScale = _uiVisible ? Vector3.one : Vector3.zero;
+        }
+    }
+
+    public static IEnumerable<GameObject> FindGameObjectsInLayer(int layer)
+    {
+        var goList = new List<GameObject>();
+
+        if (FindObjectsOfType(typeof(GameObject)) is GameObject[] goArray)
+        {
+            goList.AddRange(goArray.Where(obj => obj.layer == layer));
+        }
+
+        return goList.ToArray();
     }
 
     public static void ToMainMenu()
